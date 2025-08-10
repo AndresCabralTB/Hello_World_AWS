aa2c2dcd356ea3879d45e157d4e99ac493fe80b9
AndresCabralTB <andres.cabralrs@udlap.mx>
2025-08-10 02:34:20 -0600

    Commit Node.js

--- src/Node.js
+++ src/Node.js
@@ -37,7 +37,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host     : 'first-database-test-1.ccta2ec4yvaj.us-east-1.rds.amazonaws.com',
   port     : '3306',
-  user     : 'acabralreyes',
+  user     : 'admin',
   password : secret,
   database : 'new_schema'
 
@@ -50,5 +50,19 @@ console.error('Error connecting to MySQL database:', error);
 console.log('Connected to MySQL database!');
 }
 });
+
+// let sql = "INSERT INTO first_table(Name, Lastname, ID) VALUES ('Bingo', 'Cabral', '005')";
+let sql = `
+  INSERT INTO first_table (Name, Lastname, ID)
+  VALUES 
+    ('Helena', 'Reyes', '001'),
+    ('Joaquín', 'Cabral P', '002')
+`;
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+
+
 // close the MySQL connection
 connection.end();
